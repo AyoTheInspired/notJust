f55d985c79a71c6b6a781cb94097f34cc1a9f0f8
Ayo <ayoinspired@gmail.com>
2022-09-17 17:19:17 +0100

    Refactored App.js

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import FeedPost from "./src/components/FeedPost";
+import posts from "./assets/data/posts.json";
 
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<FeedPost />
+			<FlatList
+				data={posts}
+				renderItem={({ item }) => <FeedPost post={item} />}
+			/>
 			<StatusBar style="auto" />
 		</View>
 	);
